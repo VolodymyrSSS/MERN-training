d910a9a6d515ea3adb993d4f7a421d028ed1f6e8
Volodymyr <wolodymyrs@gmail.com>
2020-11-14 22:04:45 +0200

    authorisation completed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express')
 const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator')
 const User = require('../models/User')
 const router = Router()
@@ -68,6 +70,14 @@ router.post(
       if(!isMatch) {
         return res.status(400).json({message: 'The password is NOT correct! Try again.'})
       }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      )
+
+      res.status(200).json({token, userId: user.id})
 
     } catch(e){
       res.status(500).json({ message: 'Something wrong! Try again!' })
